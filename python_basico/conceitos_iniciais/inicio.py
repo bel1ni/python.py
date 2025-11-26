1aa5fbef5b5c05d65dc3ef2bd396ca59b28cfe5b
Mariane Ramalho Belini <mariane.r.belini@gmail.com>
2025-11-26 14:37:13 -0300

    Add files via upload

---
+++ python_basico/conceitos_iniciais/inicio.py
@@ -0,0 +1,7 @@
+#print('OLÁ, MUNDO!')#
+'''tipo string'''
+#print(type('10'))#
+'''O TYPE mostra a classe de alguma coisa
+O que recebe TRUE e FALSE é boolean
+O // faz divisão e só mostra o número inteiro caso dê número quebrado
+'''
---
+++ python_basico/conceitos_iniciais/variaveis.py
@@ -0,0 +1,21 @@
+
+#nome='Mariane'
+#idade=17
+#print(nome)
+#print(idade)
+'''Variável sempre deve começar com uma letra ou com _
+Não pode começar com número.
+Letra minúscula e maiúscula contam.'''
+
+#primeiro_numero=7
+#segundo_numero=10
+#resultado=primeiro_numero+segundo_numero
+#print(resultado)
+
+#nome=input('qual é o seu nome? ')
+#print(f'Prazer em te conhecer, {nome}.')
+
+primeironumero=int(input('Digite um número: '))
+segundonumero=int(input('Digite outro número: '))
+resultado=primeironumero+segundonumero
+print(f'A soma entre {primeironumero} e {segundonumero} é {resultado}.')
