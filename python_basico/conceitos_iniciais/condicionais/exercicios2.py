7e7fb22d8f73d8f0f455d4c2c20c74e517692e32
Mariane Ramalho Belini <mariane.r.belini@gmail.com>
2025-11-27 13:46:14 -0300

    Add files via upload

---
+++ python_basico/conceitos_iniciais/condicionais/exercicios2.py
@@ -0,0 +1,12 @@
+#nota=float(input('Digite sua nota: '))
+#if nota>=60:
+#    print('Você atingiu a nota mínima!')
+
+#n1=input('Digite um nome: ')
+#n2=input('Digite outro nome: ')
+#if n1 != n2:
+#    print(f'Os nomes {n1} e {n2} são diferentes.')
+
+idade=int(input('Digite sua idade: '))
+if idade >=15:
+    print('Você pode participar do evento!')
---
+++ python_basico/conceitos_iniciais/condicionais/if_else.py
@@ -0,0 +1,14 @@
+while True:
+    #idade=int(input('Qual é sua idade? '))
+    #if idade > 17:
+        #print('Você é maior de idade')
+    #else:
+        #print('Você é menor de idade')
+    
+    '''senha= 'jose'
+    tentativa_senha=input('Digite a senha: ')
+    if tentativa_senha == senha:
+        print('Acesso liberado')
+        else:
+        print('Acesso negado')'''
+    
